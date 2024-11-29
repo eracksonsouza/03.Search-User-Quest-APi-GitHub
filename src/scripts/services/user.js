94a6ed38910a53d64be38c0195bc689f95d9d9b6
Erackson Souza <souza.erackson@gmail.com>
2024-11-28 21:10:22 -0300

    fix: Added fix to the error that did not show the github user.

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,12 +1,9 @@
-import { baseUrl } from "/src/scripts/variables.js"
+import { baseUrl } from "../variables.js"
 
-async function getUser(userName){
-    //essa API faz com que o número de requisições do usuario seja "Burlada" , e que não tenha número máximo de requisições 
-    const response = await fetch (`https://api.allorigins.win/get?url=${baseUrl}/${userName}`)
-    const result = await response.json()
-    // console.log('result', JSON.parse(result.contents));
-    //json.parse pega uma coisa que vem em stringfy e transforma em objeto, ou seja, vai pegar um string em objeto
-    return JSON.parse(result.contents)
+// Função assíncrona para chamar o EndPoint da API do GitHub de nome do usuário.
+async function getUser(userName) {
+    const response = await fetch(`${baseUrl}/${userName}`);
+    return await response.json()
 }
 
 export { getUser }
